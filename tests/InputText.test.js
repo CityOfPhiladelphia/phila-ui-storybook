7b3555370a5cda020ce9c495e6a964bb60549a07
Dan Lopez <dan.lopez@phila.gov>
2019-09-13 13:47:52 -0400

    Adds Jest to project, initial tests (#4)
    Adds Jest Framework, test for InputText.vue

---
+++ tests/InputText.test.js
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils';
+import InputText from  '../components/InputText.vue';
+
+
+describe('InputText', () => {
+  it('renders the correct markup', () => {
+    const wrapper = mount(InputText);
+    expect(wrapper.html()).toContain('<div class="input-wrap input-text">');
+  });
+  it('renders the placeholder text and label based on placeholder', () => {
+    const wrapper = mount(InputText, {
+      propsData:  {
+        placeholder: 'Philadelphia Sports Mascot',
+        value: 'Gritty'
+      }
+    });
+    expect(wrapper.find('label').text()).toEqual('Philadelphia Sports Mascot');
+    expect(wrapper.find('input').attributes('placeholder')).toEqual('Philadelphia Sports Mascot');
+  });
+});
